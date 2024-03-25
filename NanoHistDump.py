3312e07bac94c8ea2f642e05f553c18c57bb8e75
pviscone <pieroviscone@gmail.com>
2024-03-25 01:56:13 +0100

    Added main argparsing script

--- NanoHistDump.py
+++ NanoHistDump.py
@@ -1,6 +1,54 @@
-def NanoHistDump():
-    pass
+import importlib
+import os
+
+import typer
+import yaml
+
+from python.sample import sample_generator
+
+
+def NanoHistDump(
+    config_file: str = typer.Option(..., "-f", "--file", help="specify the python configuration file"),
+    dataset_file: str = typer.Option(
+        ..., "-i", "--input-dataset", help="specify the yaml file defining the input dataset"
+    ),
+    samples: str = typer.Option(
+        None,
+        "-s",
+        "--sample",
+        help="specify the samples to be processed, separate them by commas if more than one is needed (default all samples)",
+    ),
+    out_dir: str = typer.Option(None, "-o", "--out_dir", help="override the output directory for the files"),
+    nevents: int = typer.Option(None, "-n", "--nevents", help="number of events to process per sample (default all)"),
+):
+    def parse_yaml(filename):
+        with open(filename) as stream:
+            return yaml.load(stream, Loader=yaml.FullLoader)
+
+    dataset = parse_yaml(dataset_file)
+
+    if out_dir is not None:
+        dataset["dataset"]["out_dir"] = out_dir
+    os.makedirs(dataset["dataset"]["out_dir"], exist_ok=True)
+
+    if samples is not None:
+        samples = samples.split(",")
+        if len(set(samples) - set(dataset["samples"])) > 0:
+            raise ValueError(
+                f"Samples {set(samples)-set(dataset['samples'])} not found in the dataset\nAvailable samples: {dataset['samples'].keys()}"
+            )
+
+        dataset["samples"] = {sample: dataset["samples"][sample] for sample in samples}
+
+    cfg = importlib.import_module(config_file.split(".py")[0].replace("/", "."))
+
+    for sample in sample_generator(dataset):
+        if nevents is not None:
+            sample = sample[:nevents]  # noqa: PLW2901
+        cfg.define(sample)
+        sample.create_outfile(dataset["dataset"]["out_dir"])
+        sample.add_hists(cfg.hists)
 
 
 if __name__ == "__main__":
-    pass
+    typer.run(NanoHistDump)
