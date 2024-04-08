a40c3e7b9bb807781df5830d06daea40e50faa87
pviscone <pieroviscone@gmail.com>
2024-04-08 11:48:21 +0200

    test script

--- root_files/test.py
+++ root_files/test.py
@@ -1,154 +1,93 @@
-
 # %%
 import importlib
 import sys
 
 import awkward as ak
-import dask_awkward as dak
 import numpy as np
-from coffea.nanoevents import NanoAODSchema, NanoEventsFactory
 
 sys.path.append("..")
 import python.sample
+from cfg.functions.matching import count_matched, match_to_gen, select_matched
+from cfg.functions.utils import add_collection
 
 importlib.reload(python.sample)
 Sample = python.sample.Sample
 
-
-NanoAODSchema.warn_missing_crossrefs = False
-
+path = "/data/pviscone/CMSSW/CMSSW_14_0_0_pre3/src/FastPUPPI/NtupleProducer/NanoNizer/NanoDoubleElectrons/*.root"
 fname = "DoubleElectrons/DoubleElectrons.root"
-events = NanoEventsFactory.from_root(
-    {fname: "Events"},
-    schemaclass=NanoAODSchema,
-).events()
+path = "DoubleElectrons"
+scheme = {"CaloEGammaCrystalClustersGCT": "CryClu", "GenEl": "GenEle", "DecTkBarrel": "Tk", "TkEleL2": "TkEle"}
+
 BarrelEta = 1.479
-events=Sample("sample",events=events)
-""" events["GenEl"] = events.GenEl[np.abs(events.GenEl.eta) < BarrelEta]
-events["GenEl"] = events.GenEl[events.GenEl.pt > 5]
-events=events.filter(dak.num(events.GenEl)>0) """
 
-gen = events.GenEl
-obj = events.CaloEGammaCrystalClustersGCT
-dr_cut = 0.1
-calovar = False
+events = Sample("example", path=path, tree_name="Events", scheme_dict=scheme, nevents=1000)
+events = events.events
+add_collection(events, "n")
+events["GenEle"] = events.GenEle[np.abs(events.GenEle.eta) < BarrelEta]
+# events["GenEle"] = events.GenEle[events.GenEle.pt > 5]
+events = events[ak.num(events.GenEle) > 0]
 
+#!-------------------CryClu-Gen Matching-------------------!#
+events["CryCluGenMatchedAll"] = match_to_gen(events.CryClu, events.GenEle, calovar=True)
+events["CryCluGenMatched"] = select_matched(events.CryCluGenMatchedAll)
+events["n", "CryCluGenMatchedAll"], events["n", "CryCluGenMatchedAllPt"] = count_matched(
+    events.CryCluGenMatchedAll, events.GenEle
+)
 
+#!-------------------Tk-Gen Matching-------------------!#
+events["TkGenMatchedAll"] = match_to_gen(events.Tk, events.GenEle)
+events["TkGenMatched"] = select_matched(events.TkGenMatchedAll)
+events["n", "TkGenMatchedAll"], events["n", "TkGenMatchedAllPt"] = count_matched(events.TkGenMatchedAll, events.GenEle)
 
+#!-------------------TkEle-Gen Matching-------------------!#
+events["TkEleGenMatchedAll"] = match_to_gen(events.TkEle, events.GenEle, calovar=True)
+events["TkEleGenMatched"] = select_matched(events.TkEleGenMatchedAll)
+events["n", "TkEleGenMatchedAll"], events["n", "TkEleGenMatchedAllPt"] = count_matched(
+    events.TkEleGenMatchedAll, events.GenEle
+)
 
-
-def count_match(matched_objs, gen):
-    print("Count")
-    obj = matched_objs.compute()
-    genobj = gen.compute()
-    max_idx = ak.max(obj.genIdx)
-    genobj = ak.pad_none(genobj, max_idx + 1)
-    for i in range(max_idx + 1):
-        matched_to_i = obj[obj.genIdx == i]
-        matched_to_i = ak.drop_none(matched_to_i)
-        n = ak.singletons(ak.num(matched_to_i))
-        pt = ak.singletons(genobj.pt[:, i])
-        if i == 0:
-            # extend dims
-            n_matched = n
-            pt_matched = pt
-        else:
-            n_matched = ak.concatenate([n_matched, n], axis=1)
-            pt_matched = ak.concatenate([pt_matched, pt], axis=1)
-    return dak.from_awkward(n_matched, 1), dak.from_awkward(pt_matched, 1)
+# %%
 
 
+def obj2obj_match(names, obj1_to_match, obj2_to_match, dr_cut=0.2, var=None):
+    name1, name2 = names
+    obj1_to_match = ak.with_name(obj1_to_match, "Momentum4D")
+    obj2_to_match = ak.with_name(obj2_to_match, "Momentum4D")
+    if var is not None:
+        obj1_to_match["eta"] = obj1_to_match[var[0]["eta"]]
+        obj1_to_match["phi"] = obj1_to_match[var[0]["phi"]]
+        obj2_to_match["eta"] = obj2_to_match[var[1]["eta"]]
+        obj2_to_match["phi"] = obj2_to_match[var[1]["phi"]]
 
-def gen_match(obj, gen, dr_cut=0.1, calovar=False):
-    gen_to_match = gen.compute()
-    obj_to_match = obj.compute()
-    if calovar:
-        gen_to_match["eta"] = gen_to_match.caloeta
-        gen_to_match["phi"] = gen_to_match.calophi
-    gen_to_match = ak.with_name(gen_to_match, "PtEtaPhiMLorentzVector")
-    obj_to_match = ak.with_name(obj_to_match, "PtEtaPhiMLorentzVector")
-
-    n = ak.max(ak.num(gen_to_match, axis=1))
-    gen_to_match = ak.pad_none(gen_to_match, n)
+    n = ak.max(ak.num(obj2_to_match, axis=1))
+    obj2_to_match = ak.pad_none(obj2_to_match, n)
     for i in range(n):
-        dr = gen_to_match[:, i].delta_r(obj_to_match)
-        matched_obj = obj_to_match[dr < dr_cut]
-        # argmax = ak.argmax(matched_obj.pt, axis=1, keepdims=True)
-        # matched_obj = matched_obj[argmax]
-        matched_obj["genPt"] = gen_to_match[:, i].pt
-        matched_obj["genEta"] = gen_to_match[:, i].eta
-        matched_obj["genPhi"] = gen_to_match[:, i].phi
-        matched_obj["genIdx"] = i
-        matched_obj["dPt"] = matched_obj.pt - gen_to_match[:, i].pt
-        matched_obj["dR"] = dr[dr < dr_cut]  # [argmax]
+        dr = obj2_to_match[:, i].deltaR(obj1_to_match)
+        matched_obj = obj1_to_match[dr < dr_cut]
+        for fields2 in obj2_to_match.fields:
+            if "gen" in fields2:
+                matched_obj[f"{fields2}"] = obj2_to_match[:, i][fields2]
+            else:
+                matched_obj[f"{name2}_{fields2}"] = obj2_to_match[:, i][fields2]
+        matched_obj[f"{name2}Idx"] = i
+        for fields1 in obj1_to_match.fields:
+            idx = matched_obj.layout.content.fields.index(fields1)
+            matched_obj.layout.content.fields[idx] = f"{name1}_{fields1}"
+        matched_obj[f"{name1}_{name2}_dR"] = dr[dr < dr_cut]
+        matched_obj[f"{name1}_{name2}_dPt"] = matched_obj[f"{name1}_pt"] - obj2_to_match[:, i].pt
+        matched_obj["pt"] = matched_obj[f"{name2}_pt"]
+        matched_obj["eta"] = matched_obj[f"{name2}_eta"]
+        matched_obj["phi"] = matched_obj[f"{name2}_phi"]
         if i == 0:
             matched_objs = matched_obj
         elif i > 0:
             matched_objs = ak.concatenate([matched_objs, matched_obj], axis=1)
-    return dak.from_awkward(matched_objs, 1)
+    return matched_objs
 
-sample=events
-sample["GenEle"] = sample.GenEl
-sample["CryClu"] = sample.CaloEGammaCrystalClustersGCT
-sample.add_collection("n")
-#!-------------------GEN Selection-------------------!#
-sample["GenEle"] = sample.GenEle[np.abs(sample.GenEle.eta) < BarrelEta]
-sample["GenEle"] = sample.GenEle[sample.GenEle.pt > 5]
-sample=sample.filter(dak.num(sample.GenEle) > 0)
 
-#!-------------------CryClu-Gen Matching-------------------!#
-sample["CryCluGenMatchedAll"] = gen_match(sample.CryClu, sample.GenEle, calovar=True)
-sample["n", "CryCluGenMatchedAll"], sample["n", "CryCluGenMatchedAllPt"] = count_match(
-    sample.CryCluGenMatchedAll, sample.GenEle
+events["TkCryCluMatch"] = obj2obj_match(
+    ["Tk", "CryCluGenMatchedAll"],
+    events.Tk,
+    events.CryCluGenMatchedAll,
+    var=[{"eta": "caloEta", "phi": "caloPhi"}, {"eta": "eta", "phi": "phi"}],
 )
-
-#!!!!!!!!!!!!!!!TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTt
-# %%
-
-gen_to_match = gen.compute()
-obj_to_match = obj.compute()
-if calovar:
-    gen_to_match["eta"] = gen_to_match.caloeta
-    gen_to_match["phi"] = gen_to_match.calophi
-gen_to_match = ak.with_name(gen_to_match, "PtEtaPhiMLorentzVector")
-obj_to_match = ak.with_name(obj_to_match, "PtEtaPhiMLorentzVector")
-
-n = ak.max(ak.num(gen_to_match, axis=1))
-gen_to_match = ak.pad_none(gen_to_match, n)
-for i in range(n):
-    dr = gen_to_match[:, i].delta_r(obj_to_match)
-    matched_obj = obj_to_match[dr < dr_cut]
-    #argmax = ak.argmax(matched_obj.pt, axis=1, keepdims=True)
-    #matched_obj = matched_obj[argmax]
-    matched_obj["genPt"] = gen_to_match[:, i].pt
-    matched_obj["genEta"] = gen_to_match[:, i].eta
-    matched_obj["genPhi"] = gen_to_match[:, i].phi
-    matched_obj["genIdx"] = i
-    matched_obj["dPt"] = matched_obj.pt - gen_to_match[:, i].pt
-    matched_obj["dR"] = dr[dr < dr_cut]#[argmax]
-    if i == 0:
-        matched_objs = matched_obj
-    elif i > 0:
-        matched_objs = ak.concatenate([matched_objs, matched_obj], axis=1)
-    # return dak.from_awkward(matched_objs, 1)
-#%%
-#def count(obj):
-
-obj = matched_objs#.compute()
-genobj = gen.compute()
-max_idx = ak.max(obj.genIdx)
-genobj = ak.pad_none(genobj, max_idx + 1)
-for i in range(max_idx + 1):
-    matched_to_i = obj[obj.genIdx == i]
-    matched_to_i = ak.drop_none(matched_to_i)
-    n = ak.singletons(ak.num(matched_to_i))
-    pt = ak.singletons(genobj.pt[:, i])
-    if i == 0:
-        # extend dims
-        n_matched = n
-        pt_matched = pt
-    else:
-        n_matched = ak.concatenate([n_matched, n], axis=1)
-        pt_matched = ak.concatenate([pt_matched, pt], axis=1)
-
-
