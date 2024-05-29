0c08c044cd5ae3d66e27796e26987b9ac3f27443
pviscone <pieroviscone@gmail.com>
2024-05-29 21:45:50 +0200

    elliptic matching studies

--- cfg/functions/matching.py
+++ cfg/functions/matching.py
@@ -183,6 +183,52 @@ def match_obj_to_couple(obj, couple, to_compare, dr_cut=0.2, etaphi_vars=(("eta"
     return cart
 
 
+
+
+def delta_phi(phi1, phi2):
+    dphi = phi1 - phi2
+    dphi_over_pi = dphi > np.pi
+    dphi_under_neg_pi = dphi < -np.pi
+    dphi = np.where(dphi_over_pi, dphi - 2 * np.pi, dphi)
+    dphi = np.where(dphi_under_neg_pi, dphi + 2 * np.pi, dphi)
+    return dphi
+
+def elliptic_match(obj1,obj2,etaphi_vars,ellipse=None):
+    cart, name1, name2 = cartesian(obj1, obj2,nested=True)
+
+    obj1_name=etaphi_vars[0][0].split("/")[:-1]
+    obj2_name=etaphi_vars[1][0].split("/")[:-1]
+    phi1=cart[name1][*etaphi_vars[0][1].split("/")]
+    eta1=cart[name1][*etaphi_vars[0][0].split("/")]
+    phi2=cart[name2][*etaphi_vars[1][1].split("/")]
+    eta2=cart[name2][*etaphi_vars[1][0].split("/")]
+
+    dphi=delta_phi(phi1,phi2)
+    deta=eta1-eta2
+
+    #if ellipse is number
+    if isinstance(ellipse,int|float):
+        mask=(dphi**2/ellipse**2+deta**2/ellipse**2)<1
+
+    elif isinstance(ellipse,tuple|list):
+        if isinstance(ellipse[0],int|float) and isinstance(ellipse[1],int|float):
+            mask=(dphi**2/ellipse[1]**2+deta**2/ellipse[0]**2)<1
+        else:
+            mask_arr=[(dphi**2/ellipse_element[1]**2+deta**2/ellipse_element[0]**2)<1 for ellipse_element in ellipse]
+
+            mask=dphi>666
+            for elem in mask_arr:
+                mask=np.bitwise_or(mask,elem)
+    cart=cart[mask]
+    cart["dR"]=np.sqrt(dphi[mask]**2+deta[mask]**2)
+    cart["dPt"]=cart[name1][*(obj1_name+["pt"])]-cart[name2][*(obj2_name+["pt"])]
+    cart["dEta"]=deta[mask]
+    cart["dPhi"]=dphi[mask]
+    return ak.drop_none(cart)
+
+
+
+
 def match_obj_to_obj(obj, couple, dr_cut=0.2, etaphi_vars=(("eta", "phi"), ("eta", "phi")),nested=False):
     obj2_to_match = couple
     obj_to_match = obj
