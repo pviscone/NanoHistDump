d89ee5fe10dbc8194102b04156c0b93f70e148ab
Piero Viscone <piero.viscone@cern.ch>
2024-09-03 03:52:19 +0200

    minors

--- models/Barrel/coniferModel.py
+++ models/Barrel/coniferModel.py
@@ -1,6 +1,7 @@
 # %%
 import os
 import sys
+import numpy as np
 
 sys.path.append("/afs/cern.ch/work/p/pviscone/conifer/conifer")
 sys.path.append("../")
@@ -20,10 +21,10 @@ import plots
 
 hep.style.use("CMS")
 
-classes = 3
+classes = 2
 
-test_file = "/afs/cern.ch/work/p/pviscone/NanoHistDump/models/flatBDT/dataset/131Xv3_test.parquet"
-model = f"light{classes}_131Xv3.json"
+test_file = "/afs/cern.ch/work/p/pviscone/NanoHistDump/models/pandas_dataset/131Xv3_test.parquet"
+model = f"barrel{classes}classes_131Xv3.json"
 
 features = [
     "CryClu_pt",
@@ -39,29 +40,30 @@ features = [
     "abs_dPhi",
 ]
 
-scaler = BitScaler()
-scaler.load("scaler.npy")
+#scaler = BitScaler()
+#scaler.load("scaler.npy")
+scaler=None
 
-df_test, dtest = load_parquet(test_file, features, scaler=scaler, ptkey="CC_pt", label2=2 if classes > 2 else 1)
+df_test, dtest, _ = load_parquet(test_file, features, scaler=scaler, ptkey="CC_pt", label2=2 if classes > 2 else 1)
 
 xgbmodel = xgb.Booster()
 xgbmodel.load_model(model)
 
 build = False
 
-
+#%%
 #!----------------------VIVADO ENVS----------------------!#
 os.environ["PATH"] = "/home/Xilinx/Vivado/2023.1/bin:/home/Xilinx/Vitis_HLS/2023.1/bin:" + os.environ["PATH"]
 os.environ["XILINX_AP_INCLUDE"] = "/opt/Xilinx/Vitis_HLS/2023.1/include"
 os.environ["JSON_ROOT"] = "/afs/cern.ch/work/p/pviscone/conifer"
 
 #!----------------------CFG----------------------!#
-backend = "cpp"
-
+backend = "vivado"
+precision = "ap_fixed<30,19>"
 if backend == "vivado":
     cfg = conifer.backends.xilinxhls.auto_config()
     cfg["XilinxPart"] = "xcvu13p-flga2577-2-e"
-    cfg["Precision"] = "ap_fixed<64,32>"
+    cfg["Precision"] = precision
 
 
 elif backend == "py":
@@ -69,9 +71,9 @@ elif backend == "py":
 
 elif backend == "cpp":
     cfg = conifer.backends.cpp.auto_config()
-    cfg["Precision"] = "float"
+    cfg["Precision"] = precision
 
-cfg["OutputDir"] = "conifer_prj"
+cfg["OutputDir"] = f"conifer_{model.replace('.json', '')}"
 
 
 def convert_and_evaluate(model, dmatrix, cfg, name, save=False):
@@ -81,7 +83,7 @@ def convert_and_evaluate(model, dmatrix, cfg, name, save=False):
     hls_model.compile()
     xgbpreds = 1 - model.predict(dmatrix)[:, 0] if classes > 2 else model.predict(dmatrix)
     hls_preds = hls_model.decision_function(dmatrix.get_data().toarray())
-    hls_preds = 1 - softmax(hls_preds)[:, 0] if classes > 2 else hls_preds
+    hls_preds = 1 - softmax(hls_preds)[:, 0] if classes > 2 else 1/(1+np.exp(-hls_preds))
     fpr, tpr, _ = roc_curve(y, xgbpreds)
     hlsfpr, hlstpr, _ = roc_curve(y, hls_preds)
     plt.plot(fpr, tpr, label=f"XGBoost {name}")
@@ -96,10 +98,12 @@ def convert_and_evaluate(model, dmatrix, cfg, name, save=False):
 
 
 # %%
-#!----------------------Light model----------------------!#
+#!----------------------Conifer model----------------------!#
+import os
+os.makedirs(cfg["OutputDir"]+"/fig",exist_ok=True)
 
 hlsmodel, df_test["XGBScore"], df_test["HLSScore"] = convert_and_evaluate(
-    xgbmodel, dtest, cfg, "", save=f"fig/class{classes}/conifer_rocs.pdf"
+    xgbmodel, dtest, cfg, "", save=cfg['OutputDir']+"/fig/conifer_rocs.pdf"
 )
 
 # %%
@@ -111,13 +115,12 @@ if build:
 
 # %%
 
-
 plots.plot_best_pt_roc(
     df_test,
     ptkey="CC_pt",
     score="HLSScore",
     thrs_to_select=[0.85, 0.7, 0.35, 0.3, 0.55, 0.85],
-    save=f"fig/class{classes}/hls_pt_roc.pdf",
+    save=cfg['OutputDir']+"/fig/hls_pt_roc.pdf",
 )
 
 
@@ -126,5 +129,5 @@ plots.plot_best_pt_roc(
     ptkey="CC_pt",
     score="XGBScore",
     thrs_to_select=[0.85, 0.7, 0.35, 0.3, 0.55, 0.85],
-    save=f"fig/class{classes}/xgb_pt_roc.pdf",
+    save=cfg['OutputDir']+"/fig/xgb_pt_roc.pdf",
 )
