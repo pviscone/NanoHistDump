413ee88bf8764c32181787f815b0c5394ce661f1
Piero Viscone <piero.viscone@cern.ch>
2024-09-12 00:39:15 +0200

    new envs

--- envs.py
+++ envs.py
@@ -6,5 +6,5 @@ def set_envs():
     sys.path.append("/afs/cern.ch/work/p/pviscone/conifer/conifer")
 
     os.environ["PATH"] = "/home/Xilinx/Vivado/2023.1/bin:/home/Xilinx/Vitis_HLS/2023.1/bin:" + os.environ["PATH"]
-    os.environ["XILINX_AP_INCLUDE"] = "/opt/Xilinx/Vitis_HLS/2023.1/include"
+    os.environ["XILINX_AP_INCLUDE"] = "/afs/cern.ch/work/p/pviscone/conifer/XilinxInclude"
     os.environ["JSON_ROOT"] = "/afs/cern.ch/work/p/pviscone/conifer"
