bdf5040803c6c54f2cc35e1f29d33237b6f2a943
Piero Viscone <piero.viscone@cern.ch>
2024-09-27 20:14:59 +0200

    renamed folder

--- cfg/selectors/TkEle.py
+++ cfg/selectors/TkEle.py
@@ -4,9 +4,9 @@ import awkward as ak
 BarrelEta = 1.479
 
 def select(events, key, region=None):
-    if region == "Barrel":
+    if region.lower() == "barrel":
         events[key] = events.TkEle[np.abs(events[key,"eta"]) < BarrelEta]
-    elif region == "Endcap":
+    elif region.lower() == "endcap":
         events[key] = events.TkEle[np.bitwise_and(np.abs(events[key,"eta"]) >= BarrelEta, np.abs(events[key,"eta"]) < 2.4)]
     events[key, "hwQual"] = ak.values_astype(events[key].hwQual, np.int32)
     mask_tight_ele = 0b0010
